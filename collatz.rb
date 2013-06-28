655583b94cc752b978293bb7f7e18ff120c070fd
Jacob Smith <jacob.wesley.smith@gmail.com>
2013-06-28 21:07:41 +0000

    Hash successfully implemented...I think

--- collatz.rb
+++ collatz.rb
@@ -5,39 +5,50 @@ $recorded_iterations = {}
 $init = 0
 $steps = 0
 
-def collatz(n)
-#	if $recorded_iterations[n]
-#		$recorded_iterations[init] = $recorded_iterations[n] + steps
-#	end
+def collatz(n, step)
+	if $recorded_iterations[n]
+		return n, $recorded_iterations[n] + step 
+	end
 
 	if n == 1
-		return n
+		step += 1
+		return n, step
 	elsif n % 2 == 0
+		step += 1
 #		puts n
-		collatz(n/2)
+		collatz((n/2), step)
 	else
+		step += 1
 #		puts n
-		collatz((3*n)+1)
+		collatz(((3*n)+1), step)
 	end
-	$steps += 1
+#	$steps += 1
 end
 
 
 input = ARGV[0].to_i
 max_steps = 0
 max_num = 0
+
+
 1.upto(input).each do |i|
-
-
-	collatz(i)
-	if max_steps < $steps
-		max_steps = $steps
+		#initialize step counter to 0
+	step = 0
+	step_count = 0
+	number, step_count =	collatz(i, step)
+#		puts i, step_count
+#		puts '--'
+		$recorded_iterations[i] = step_count
+	if max_steps < step_count 
+		max_steps = step_count
 		max_num = i
 	end	
 
 
-	$steps = 0
+
 end
 
 puts max_steps
-puts max_num 
+puts max_num
+
+# puts $recorded_iterations 
