6b2b941f01e88efbc2a3b7758b891fd542aade01
Jacob Smith <jacob.wesley.smith@gmail.com>
2013-06-29 03:44:17 +0000

    Tried to clean up collatz.rb a bit--needs more work

--- collatz.rb
+++ collatz.rb
@@ -15,14 +15,14 @@ def collatz(n, step)
 		return n, step
 	elsif n % 2 == 0
 		step += 1
-#		puts n
+
 		collatz((n/2), step)
 	else
 		step += 1
-#		puts n
+
 		collatz(((3*n)+1), step)
 	end
-#	$steps += 1
+
 end
 
 
@@ -32,15 +32,19 @@ max_num = 0
 
 
 1.upto(input).each do |i|
-		#initialize step counter to 0
+		#initialize step counter to 0 (carries the step count within the iterations of a given 'i')
 	step = 0
-	step_count = 0
-	number, step_count =	collatz(i, step)
-#		puts i, step_count
-#		puts '--'
-		$recorded_iterations[i] = step_count
-	if max_steps < step_count 
-		max_steps = step_count
+
+		#total_step_count is the sum of the internal step count, plus whatever was found in the hash lookup of $recorded_iterations
+	total_step_count = 0
+
+	number, total_step_count =	collatz(i, step)
+	
+
+		# add the given 'i' to our hash of outputs to use on the next lookup
+	$recorded_iterations[i] = total_step_count
+	if max_steps < total_step_count 
+		max_steps = total_step_count
 		max_num = i
 	end	
 
@@ -48,7 +52,7 @@ max_num = 0
 
 end
 
-puts max_steps
-puts max_num
 
-# puts $recorded_iterations 
+puts max_num.to_s + " is the number with the longest chain (" + max_steps.to_s + " total steps). "
+
+
